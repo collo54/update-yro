3ba1c8a2940be6af8a96e199c719c183106a25c6
Collins Mutugi <mutugicollins54@gmail.com>
2022-06-01 22:29:48 +0300

    ui implementation

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:update_yro/constants/colors.dart';
 import 'package:update_yro/custom/data_search.dart';
 
 class Homecontent extends StatelessWidget {
@@ -15,16 +16,7 @@ class Homecontent extends StatelessWidget {
         child: _buildTop(context),
         width: width,
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment(
-                0.8, 0.0), // 10% of the width, so there are ten blinds.
-            colors: <Color>[
-              Color.fromRGBO(243, 243, 251, 1),
-              Color.fromRGBO(253, 251, 253, 1)
-            ], // red to yellow
-            tileMode: TileMode.repeated, // repeats the gradient over the canvas
-          ),
+          color: Colors.white,
         ),
       ),
     ];
@@ -57,10 +49,61 @@ class Homecontent extends StatelessWidget {
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  child: const Icon(Icons.menu),
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Welcome, jim',
+                  style: GoogleFonts.acme(
+                    height: 1.7,
+                    textStyle: const TextStyle(
+                      color: kwelcomejim,
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 100,
+                ),
+                const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.shopping_basket_rounded,
+                    color: kOrange,
+                  ),
+                ),
+              ],
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(1),
+                topRight: Radius.circular(1),
+                bottomLeft: Radius.circular(1),
+                bottomRight: Radius.circular(1),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 2),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 GestureDetector(
-                  child: Image.asset("assets/images/icons8-search-30(1).png"),
+                  child: const Icon(Icons.search),
                   onTap: () {
                     showSearch(context: context, delegate: DataSearch());
                   },
@@ -69,19 +112,152 @@ class Homecontent extends StatelessWidget {
                   width: 5,
                 ),
                 Text(
-                  'search',
-                  style: GoogleFonts.actor(
+                  'search for spices',
+                  style: GoogleFonts.acme(
+                    height: 1.3,
                     textStyle: const TextStyle(
                       color: Color.fromARGB(255, 145, 146, 146),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                      fontWeight: FontWeight.normal,
                     ),
                   ),
                 ),
               ],
             ),
             decoration: const BoxDecoration(
-              color: Colors.white,
+              color: ktextfill,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 2),
+          child: Container(
+            height: 40,
+            child: ListView(
+              // This next line does the trick.
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Chip(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(10),
+                  label: Text(
+                    'basil',
+                    style: GoogleFonts.acme(
+                      height: 1.14,
+                      textStyle: const TextStyle(
+                        color: kframe60,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Chip(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(10),
+                  label: Text(
+                    'curry',
+                    style: GoogleFonts.acme(
+                      height: 1.14,
+                      textStyle: const TextStyle(
+                        color: kframe60,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Chip(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(10),
+                  label: Text(
+                    'pepper',
+                    style: GoogleFonts.acme(
+                      height: 1.14,
+                      textStyle: const TextStyle(
+                        color: kframe60,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Chip(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(10),
+                  label: Text(
+                    'lavender',
+                    style: GoogleFonts.acme(
+                      height: 1.14,
+                      textStyle: const TextStyle(
+                        color: kframe60,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Chip(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(10),
+                  label: Text(
+                    'chives',
+                    style: GoogleFonts.acme(
+                      height: 1.14,
+                      textStyle: const TextStyle(
+                        color: kframe60,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Chip(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(10),
+                  label: Text(
+                    'cayanne pepper',
+                    style: GoogleFonts.acme(
+                      height: 1.14,
+                      textStyle: const TextStyle(
+                        color: kframe60,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+              ],
+            ),
+            decoration: const BoxDecoration(
+              color: kframe63,
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(1),
                 topRight: Radius.circular(1),
